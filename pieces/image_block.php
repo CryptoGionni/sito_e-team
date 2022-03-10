dbb20a0ca6bdd533f1c9a7466c3cd7995653dadf
vincenzo palomba <62468922+vincip99@users.noreply.github.com>
2022-03-10 20:28:00 +0100

    Delete pieces directory

--- pieces/image_block.php
+++
@@ -1,3 +0,0 @@
-<img src="<?= $root; ?>docs/assets/site-imgs/t-one_front.webp" class="img-fluid" alt="t-one evo">
-<img src="<?= $root; ?>docs/assets/site-imgs/t-one_side.webp" class="img-fluid" alt="t-one evo">
-<img src="<?= $root; ?>docs/assets/site-imgs/t-one_back.webp" class="img-fluid" alt="t-one evo">
