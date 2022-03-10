52c4d69155af7e863cd96313d9843b1a232bb6ad
CryptoGionni <84587835+CryptoGionni@users.noreply.github.com>
2022-03-10 19:01:15 +0100

    Add files via upload

---
+++ pieces/image_block.php
@@ -0,0 +1,3 @@
+<img src="<?= $root; ?>docs/assets/site-imgs/t-one_front.webp" class="img-fluid" alt="t-one evo">
+<img src="<?= $root; ?>docs/assets/site-imgs/t-one_side.webp" class="img-fluid" alt="t-one evo">
+<img src="<?= $root; ?>docs/assets/site-imgs/t-one_back.webp" class="img-fluid" alt="t-one evo">
---
+++ pieces/navbar.php
@@ -0,0 +1,42 @@
+
+
+<div class="container pt-4">
+  <nav class="navbar navbar-light navbar-toggleable-sm navbar-static-top text-uppercase app-navbar mb-5">
+
+    <button
+      class="navbar-toggler navbar-toggler-right hidden-md-up"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarResponsive"
+      aria-controls="navbarResponsive"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+  <!-- <div class="container-fluid"> -->
+    <a class="navbar-brand" href="<?= $root; ?>">
+      <img src="<?= $root; ?>docs/assets/home_imgs/Logo_E_Racing-01.png" alt="" width="100" height="90">
+      <!-- SQUADRA CORSE DELLA FEDERICO II -->
+    </a>
+  <!-- </div> -->
+
+
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="nav navbar-nav ml-auto">
+        <li class="nav-item ">
+          <a class="nav-link" href="<?= $root; ?>docs/minimal/t-one_evo.php">t-one evo</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="<?= $root; ?>docs/minimal/competition.php">competition</a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="<?= $root; ?>docs/minimal/partners.php">partners</a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="<?= $root; ?>docs/minimal/contact.php">contacts</a>
+        </li>
+      </ul>
+    </div><!--/.nav-collapse -->
+</nav>
+</div>
